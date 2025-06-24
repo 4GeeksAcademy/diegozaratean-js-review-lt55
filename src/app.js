d1e4716aa421e762b9347af0da74704380918a42
diego <diegozaratean@gmail.com>
2025-06-24 00:21:07 +0000

    codign guidelince

--- src/app.js
+++ src/app.js
@@ -8,4 +8,25 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  function generateRandomArray()
+  {
+    let auxArray = [];
+    let randomLength = Math.floor(Math.random()*100);// 5
+    for(let i = 0; i < randomLength; i++) auxArray.push(Math.floor(Math.random()*100));
+    return auxArray;	
+  }
+  let myStupidArray = generateRandomArray();
+
+  // Math.random() ====> 0 a 0.99999
+  // Math.random() * 100 ====> 0 a 99.999
+  // Math.floor(Math.random() * 100) ====> 0 a 99
+  // Math.ceil(Math.random() * 100) ====> 1 a 100
+  // Math.round(Math.random() * 100) ====> 0 a 100
+
+
+  let theLastOne = myStupidArray[myStupidArray.length -1]
+  console.log(theLastOne)
+  console.log(myStupidArray)
+
 };
